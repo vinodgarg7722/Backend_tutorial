f791620c7706a3c1f0ddc27f5fb3dc4a86f8d95e
vinod garg <vinodgarg070702@gmail.com>
2025-06-28 11:43:43 +0530

    Module_4(Nodejs)PracticeSet

---
+++ Nodejs/Module_4/PracticeSet/sum.js
@@ -0,0 +1,39 @@
+const sumRequestHandler = (req, res) => {
+
+    console.log("In sum Request Handler", req.url);
+
+    const body = [];
+    req.on('data', (chunk) => {
+        body.push(chunk);
+    })
+
+    req.on('end', () => {
+        const bodystr = Buffer.concat(body).toString();
+        const params = new URLSearchParams(bodystr);
+        const bodyobj = Object.fromEntries(params);
+        // const result = bodyobj.first+ bodyobj.second; // string therfore parse
+        const result = Number(bodyobj.first) + Number(bodyobj.second);
+        console.log(result)
+
+        res.setHeader('Content-Type', 'text/html');
+        res.write(`
+        <html>
+        <head><title>Practice Set</title></head>
+
+        <body>
+
+        <h1>Your Sum is ${result}</h1><br>
+        <a href = "/">Go To Home</a>
+
+        </body>
+        </html>
+        `);
+
+        return res.end();
+
+    });
+
+
+}
+
+exports.sumRequestHandler = sumRequestHandler;
